0e7d1f60e7724ce2d03859ea1225580ad259078d
Bilbia <biamcs2000@gmail.com>
2019-04-25 16:38:08 -0300

    Adicionado o sistema de colisão e os sons

--- asteroids-base.py
+++ asteroids-base.py
@@ -3,10 +3,12 @@
 # Importando as bibliotecas necessárias.
 import pygame
 import random
+import time
 from os import path
 
 # Estabelece a pasta que contem as figuras.
 img_dir = path.join(path.dirname(__file__), 'img')
+snd_dir = path.join(path.dirname(__file__), 'snd')
 
 # Dados gerais do jogo.
 WIDTH = 480 # Largura da tela
@@ -50,6 +52,9 @@ class Player(pygame.sprite.Sprite):
         #Velocidade da nave
         self.speedx = 0
         
+        #Melhora a colisão estabelecendo um raio de um circulo
+        self.radius = 25
+        
     # Metodo que atualiza a posicao da navinha
     def update(self):
         self.rect.x += self.speedx
@@ -90,6 +95,9 @@ class Mob(pygame.sprite.Sprite):
         self.speedx = random.randrange(-3,3)
         self.speedy = random.randrange(2,9)
         
+        #Melhora a colisão estabelecendo um raio de um circulo
+        self.radius = int(self.rect.width*.85/2)
+        
      # Metodo que atualiza a posicao da navinha
     def update(self):
         self.rect.x += self.speedx
@@ -114,9 +122,14 @@ pygame.display.set_caption("Asteroids")
 # Variável para o ajuste de velocidade
 clock = pygame.time.Clock()
 
-# Carrega o fundo do jogo`x
+# Carrega o fundo do jogo
 background = pygame.image.load(path.join(img_dir, 'starfield.png')).convert()
 background_rect = background.get_rect()
+
+#Carrega os sons do jogo
+pygame.mixer.music.load(path.join(snd_dir, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
+pygame.mixer.music.set_volume(0.4)
+boom_sound = pygame.mixer.Sound(path.join(snd_dir, 'expl3.wav'))
 
 # Cria uma nave. O construtor será chamado automaticamente
 player = Player()
@@ -135,6 +148,7 @@ all_sprites.add(mobs)
 try:
     
     # Loop principal.
+    pygame.mixer.music.play(loops=-1)
     running = True
     while running:
         
@@ -167,6 +181,15 @@ try:
         # Depois de processar os eventos
         # Atualiza a acao de cada sprite
         all_sprites.update()
+        
+        #Verifica se houve colisão entre nave e meteoro
+        hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
+        if hits:
+            #Toca o som da colisão
+            boom_sound.play()
+            time.sleep(1) #Precisa esperar senão fecha
+            
+            running = False
            
         
     
